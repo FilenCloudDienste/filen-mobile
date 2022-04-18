b5fcabd64a5d13b3dd2f3e6d002d0094802670e0
jontaix <31804298+jontaix@users.noreply.github.com>
2022-04-18 08:41:35 -0300

    Add Portuguese to LanguageScreen

--- src/components/LanguageScreen.js
+++ src/components/LanguageScreen.js
@@ -48,6 +48,9 @@ export const LanguageScreen = memo(({ navigation, route }) => {
                     <SettingsButtonLinkHighlight title="Italiano" onPress={() => {
                         setLang("it")
                     }} />
+                    <SettingsButtonLinkHighlight title="Português" onPress={() => {
+                        setLang("pt")
+                    }} />
                     <SettingsButtonLinkHighlight title="Русский" onPress={() => {
                         setLang("ru")
                     }} />
