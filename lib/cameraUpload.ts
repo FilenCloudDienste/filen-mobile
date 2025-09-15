576a746b4101f0fa1a3fad2ce1e8b39977aa66ba
Dwynr <dwynr@outlook.de>
2025-09-15 13:38:20 +0200

    fix(cameraUpload): rename local item if it already exists in the same path

--- lib/cameraUpload.ts
+++ lib/cameraUpload.ts
@@ -195,7 +195,18 @@ export class CameraUpload {
 				await fetch()
 
 				for (const asset of assets) {
-					const path = this.normalizePath(FileSystem.Paths.join(album.title, asset.filename))
+					let path = this.normalizePath(FileSystem.Paths.join(album.title, asset.filename))
+
+					let iteration = 1
+
+					while (items[path]) {
+						const ext = FileSystem.Paths.extname(asset.filename)
+						const name = FileSystem.Paths.basename(asset.filename, ext)
+
+						path = this.normalizePath(FileSystem.Paths.join(album.title, `${name} (${iteration})${ext}`))
+
+						iteration++
+					}
 
 					items[path] = {
 						type: "local",
